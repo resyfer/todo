d37bc4f67674cecff95a82e84271c88e0f64222e
resyfer <resyfer.dev@gmail.com>
2021-08-21 11:20:41 +0530

    Added todo js without delete

---
+++ index.js
@@ -0,0 +1,99 @@
+const submitBtn = document.getElementById('submit');
+const clearTextBtn = document.getElementById('clear-text');
+const deleteAllBtn = document.getElementById('delete-all');
+const todoArea = document.getElementById('todo-input');
+const todoContainer = document.getElementById('todo-ctnr');
+let todoItems;
+let todoDeleteBtns;
+
+if (!localStorage.getItem('todo'))
+	localStorage.setItem('todo', JSON.stringify({ todos: [] }));
+
+let { todos: todoList } = JSON.parse(localStorage.getItem('todo'));
+
+(() => {
+	for (let i = 0; i < todoList.length; i++) {
+		let todoElement = document.createElement('div');
+		makeTodoElement({
+			container: todoContainer,
+			element: todoElement,
+			value: todoList[i],
+		});
+	}
+})();
+
+addDeleteListeners();
+
+submitBtn.addEventListener('click', e => {
+	e.preventDefault();
+	if (todoArea.value) addTodo(todoArea.value);
+	todoArea.value = '';
+
+	addDeleteListeners();
+});
+
+clearTextBtn.addEventListener('click', () => {
+	todoArea.value = '';
+});
+
+deleteAllBtn.addEventListener('click', () => {
+	localStorage.setItem('todo', JSON.stringify({ todos: [] }));
+	location.reload();
+});
+
+function addTodo(todoValue) {
+	let todoNew = document.createElement('div');
+
+	makeTodoElement({
+		container: todoContainer,
+		element: todoNew,
+		value: todoValue,
+	});
+
+	todoList.push(todoValue.toString());
+	localStorage.setItem('todo', JSON.stringify({ todos: todoList }));
+}
+
+function addDeleteListeners() {
+	todoItems = document.getElementsByClassName('todo-itm');
+	todoDeleteBtns = document.getElementsByClassName('todo-itm-del');
+
+	// removeDeleteListeners();
+
+	for (let i = 0; i < todoDeleteBtns.length; i++) {
+		// todoDeleteBtns[i].addEventListener(
+		// 	'click',
+		// 	e => {
+		// 		deleteTodo(e, i);
+		// 	},
+		// 	false
+		// );
+	}
+}
+
+function removeDeleteListeners() {
+	for (let i = 0; i < todoDeleteBtns.length; i++) {
+		todoDeleteBtns[i].removeEventListener('click', deleteTodo);
+	}
+}
+
+function deleteTodo(e, i) {
+	todoList.splice(i, 1);
+	e.target.parentElement.remove();
+	localStorage.setItem('todo', JSON.stringify({ todos: todoList }));
+	// addDeleteListeners();
+}
+
+function makeTodoElement({ container, element, value }) {
+	element.setAttribute('class', 'todo-itm');
+	element.innerText = value;
+	container.appendChild(element);
+
+	addChangeOptions({ element });
+}
+
+function addChangeOptions({ element }) {
+	let deleteBtn = document.createElement('i');
+	deleteBtn.setAttribute('class', 'fas fa-trash todo-itm-del');
+	element.appendChild(deleteBtn);
+}
